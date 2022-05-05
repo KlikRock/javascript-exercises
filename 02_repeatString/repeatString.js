21cd151f31fd0134cc0d8538ed502ea7deca2eb9
Rick Klok <rickklok6@gmail.com>
2022-05-05 12:06:04 +0200

    finished lesson 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(string, num) {
+    let i = 1
+    let word = ""
+    if (num < 0) {
+        return('ERROR')
+    }
+    else {
+        while (i <= num) {     
+            word += string
+            word += ""
+            i++ 
+        }
+    return word    
+    }
+}
 // Do not edit below this line
 module.exports = repeatString;
